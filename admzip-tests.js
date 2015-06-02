c23f869640eff52c6b3c97f86bac9eb6384f6860
Pauli Jokela <pauli.jokela@didstopia.com>
2015-06-02 16:30:58 +0300

    Added proper tests

--- admzip-tests.js
+++ admzip-tests.js
@@ -1,16 +1,33 @@
-// TODO: Write actual tests
+// TODO: Where are the files being created?
+// TODO: We need to cleanup after ourselves
 
-Tinytest.add('AdmZip - Create', function (test)
+Tinytest.add('AdmZip - Create and Read a ZIP file', function(test)
 {
-  test.equal(true, true);
-});
+	// Create a new ZIP file
+	var zip = new AdmZip();
+	zip.addFile("test.txt", new Buffer("AdmZip test file."));
+	zip.writeZip('test.zip');
 
-Tinytest.add('AdmZip - Read', function (test)
-{
-  test.equal(true, true);
+	// Read the ZIP file
+	var zip = new AdmZip('test.zip');
+	var zipEntries = zip.getEntries();
+
+	// Test if the ZIP file was succesfully created/read
+	test.length(zipEntries, 1, "ZIP file length was less than 1");
 });
 
 Tinytest.add('AdmZip - Extract', function (test)
 {
-  test.equal(true, true);
+	// Create a new ZIP file
+	var zip = new AdmZip();
+	zip.addFile("test.txt", new Buffer("AdmZip test file."));
+	zip.writeZip('test.zip');
+
+	// Read and extract the ZIP file
+	var zip = new AdmZip('test.zip');	
+	zip.extractAllTo('test/', true);
+
+	// Test if the extracted file exists
+	var fs = Npm.require('fs');
+	test.equal(fs.existsSync('test/test.txt'), true, "Test file does not exist, extract failed?");
 });
--- package.js
+++ package.js
@@ -2,9 +2,13 @@ Package.describe(
 {
   name: 'didstopia:admzip',
   summary: 'Provides easy access to AdmZip.',
-  version: '0.0.2',
+  version: '0.0.3',
   git: 'https://github.com/Didstopia/meteor-admzip.git',
   documentation: 'README.md',
+  packages:
+  {
+    'maxkferg:temp': '1.0.0'
+  }
 });
 
 Npm.depends(
@@ -15,12 +19,14 @@ Npm.depends(
 Package.onUse(function(api)
 {
   api.versionsFrom('1.1.0.2');
+
   api.addFiles('admzip.js', 'server');
 });
 
 Package.onTest(function(api)
 {
-  api.use('tinytest');
-  api.use('didstopia:admzip');
-  api.addFiles('admzip-tests.js');
+  api.use(['tinytest', 'maxkferg:temp', 'didstopia:admzip']);
+
+  api.addFiles('admzip.js', 'server');
+  api.addFiles('admzip-tests.js', 'server');
 });
